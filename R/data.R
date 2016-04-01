8cb40f722c5745faa16f030753642add02a65d5a
Sven E. Templer <sven.templer@gmail.com>
2016-04-01 03:34:44 +0200

    discarded golubGO

--- R/data.R
+++ R/data.R
@@ -3,12 +3,6 @@
 #' @title Gene ontology term enrichment of differential gene expression of leukemia samples
 #' 
 #' @description 
-#' \code{golubGO} is microarray gene expression data from leukemia study of 
-#' Golub et al. (1999). Processed as described in Dudoit et al. (2002). Adapted
-#' from package \code{multtest} (see \link[multtest]{golub}). Differential gene
-#' expression was performed using a t-test to compare the two groups ALL and
-#' AML.\cr
-#' \cr
 #' \code{leukemiasGO} is microarray gene expression data from the studies of 
 #' Kohlmann et al. (2008) and Haferlach et al. (2010). Processed as described
 #' in Risueno et al. (2010). Adapted from package \code{leukemiasEset} (see
@@ -43,15 +37,6 @@
 #' 
 #' @references
 #' 
-#' Golub et al. (1999), Molecular Classification of Cancer: Class Discovery and
-#' Class Prediction by Gene Expression Monitoring, 
-#' Science, Volume 286 no. 5439 pp. 531-537, doi:10.1126/science.286.5439.531
-#' 
-#' Dudoit, Fridlyand and Speed (2002), Comparison of Discrimination Methods for
-#' the Classification of Tumors Using Gene Expression Data, 
-#' Journal of the American Statistical Association, Volume 97, Issue 457,
-#' doi:10.1198/016214502753479248
-#' 
 #' Kohlmann A, Kipps TJ, Rassenti LZ, Downing JR et al. An international 
 #' standardization programme towards the application of gene expression 
 #' profiling in routine leukaemia diagnostics: the Microarray Innovations in 
@@ -69,7 +54,6 @@
 #' 
 #' @author 
 #' Sven E. Templer [aut]\cr
-#' Authors of the multtest package [ctb]\cr
 #' Authors of the leukemiasEset [ctb]
 #' 
 
